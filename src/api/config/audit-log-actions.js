26a5a43d03fbc1fbdb27f22fd4c3d8258b0b12e4
Stefan Walther <swr@qlik.com>
2018-10-22 18:27:54 +0200

    feat: extend audit-log actions

--- src/api/config/audit-log-actions.js
+++ src/api/config/audit-log-actions.js
@@ -1,28 +1,31 @@
 module.exports = {
   SUBJECT_AUDIT_LOGS: 'audit-logs',
   cloudEvents: {
-    getRegisterEvent: () => {
+    getRegisterEvent: user_id => {
       return {
         event_domain: 'AUTH',
         event_name: 'AUTH_REGISTER',
+        user_id,
         description: 'Register account.',
         source: '/auth-service',
         action_type: 'C'
       };
     },
-    getLoginEvent: () => {
+    getLoginEvent: user_id => {
       return {
         event_domain: 'AUTH',
         event_name: 'AUTH_LOGIN',
+        user_id,
         description: 'Log into account.',
         source: '/auth-service',
         action_type: 'R'
       };
     },
-    getLogoutEvent: () => {
+    getLogoutEvent: user_id => {
       return {
         event_domain: 'AUTH',
         event_name: 'AUTH_LOGOUT',
+        user_id,
         description: 'Log out.',
         source: '/auth-service',
         action_type: 'R'
