9db4755cc7a15dc4b4801208a11452a8b0d91fc0
Stefan Walther <stefanwalther@users.noreply.github.com>
2020-07-07 22:35:21 +0200

    refactor: health-check

--- src/modules/health-check/health-check.controller.js
+++ src/modules/health-check/health-check.controller.js
@@ -2,15 +2,14 @@ const pkg = require('read-pkg-up').sync().packageJson;
 
 class HealthCheckController {
 
-  static get(req, res, next) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send({
+  static get(ctx) {
+    ctx.status = 200;
+    ctx.body = {
       ts: new Date().toJSON(),
       name: pkg.name,
       repository: pkg.repository,
       version: pkg.version
-    });
-    next();
+    };
   }
 }
 
